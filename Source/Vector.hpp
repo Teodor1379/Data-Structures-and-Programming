68fc77428dcb527385b9e178f3f22b816ee2eb70
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2024-07-08 18:24:19 +0300

    Commit: Source - Vector

--- Source/Vector.hpp
+++ Source/Vector.hpp
@@ -4,6 +4,9 @@
 #include <cstddef>
 #include <cstdlib>
 
+#include <exception>
+#include <stdexcept>
+
 template<typename T>
 class Vector {
 public:
@@ -15,14 +18,32 @@ public:
 
     Vector<T>& operator=(const Vector<T>&);
 
+    Vector<T>& operator+=(const Vector<T>&);
+    Vector<T>& operator-=(const Vector<T>&);
+    Vector<T>& operator*=(const Vector<T>&);
+    Vector<T>& operator/=(const Vector<T>&);
+    Vector<T>& operator%=(const Vector<T>&);
+
+    Vector<T> operator+(const Vector<T>&)   const;
+    Vector<T> operator-(const Vector<T>&)   const;
+    Vector<T> operator*(const Vector<T>&)   const;
+    Vector<T> operator/(const Vector<T>&)   const;
+    Vector<T> operator%(const Vector<T>&)   const;
+
     void pushBack(const T&);
     void popBack();
+
+    void sort();
 
 private:
     T*          data;
 
     std::size_t size;
     std::size_t capacity;
+
+private:
+    void allocate(const Vector<T>&);
+    void deallocate();
 };
 
 #include "Vector.inl"
